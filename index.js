a94e6ecab8e8e2009032751b72b4a1e5d4ab5ce1
Josh Chisholm <joshuachisholm@gmail.com>
2016-11-06 17:39:20 +0000

    Support languages defined in ES6 superclasses

--- index.js
+++ index.js
@@ -3,39 +3,37 @@ function promiseBuilder(delegate, promiseFactory) {
   function Proxy(promise) { this.promise = promise; }
 
   methodsOf(delegate).forEach(function(method) {
-    defineProxyMethod(Proxy, method, delegate);
+    defineProxyMethod(Proxy, method, delegate)
   })
 
   Proxy.prototype.then = function(onFulfilled, onRejected) {
-    return new Proxy(this.promise.then(onFulfilled, onRejected));
-  };
+    return new Proxy(this.promise.then(onFulfilled, onRejected))
+  }
 
   Proxy.prototype.catch = function(onRejected) {
-    return new Proxy(this.promise.catch(onRejected));
-  };
+    return new Proxy(this.promise.catch(onRejected))
+  }
 
-  return new Proxy((promiseFactory || global.Promise).resolve());
+  return new Proxy((promiseFactory || global.Promise).resolve())
 }
 
-function methodsOf(obj) {
-  var methods = [];
-  var propertyNames = Object.getOwnPropertyNames(Object.getPrototypeOf(obj));
-  for (var i = 0; i < propertyNames.length; i++) {
-    var name = propertyNames[i];
-    if (obj[name] !== obj.constructor) methods.push(name);
-  }
-  for (var name in obj) {
-    methods.push(name);
-  }
-  return methods;
+function methodsOf(object) {
+  var props = []
+  var obj = object
+  do {
+    props = props.concat(Object.getOwnPropertyNames(obj))
+  } while (obj = Object.getPrototypeOf(obj))
+  return props.filter(function (p) {
+    return p !== 'constructor' && typeof object[p] == 'function'
+  })
 }
 
 function defineProxyMethod(Proxy, method, delegate) {
   Proxy.prototype[method] = function() {
-    var args = [].slice.apply(arguments);
+    var args = [].slice.apply(arguments)
     return new Proxy(this.promise.then(function(result) {
-      return delegate[method].apply(delegate, args.concat([result]));
-    }));
+      return delegate[method].apply(delegate, args.concat([result]))
+    }))
   }
 }
 
--- test/promiseBuilderTest.js
+++ test/promiseBuilderTest.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var promiseBuilder = require('../');
-var expect = require('chai').expect;
+var promiseBuilder = require('../')
+var expect = require('chai').expect
 
 describe('promise-builder', function() {
 
-  var builder, log;
+  var builder, log
 
   beforeEach(function() {
-    log = '';
+    log = ''
   })
 
   context('with an ES5 object as the language', function() {
@@ -16,182 +16,198 @@ describe('promise-builder', function() {
     beforeEach(function() {
       builder = promiseBuilder({
         x: function() {
-          log += 'x';
-          return Promise.resolve();
+          log += 'x'
+          return Promise.resolve()
         },
         y: function() {
-          log += 'y';
-          return Promise.reject(Error('ouch'));
+          log += 'y'
+          return Promise.reject(Error('ouch'))
         },
         z: function() {
-          log += 'z';
+          log += 'z'
         },
         a: function() {
-          log += 'a';
-          return this.x();
+          log += 'a'
+          return this.x()
         },
         b: function() {
-          log += 'b';
-          throw Error('omg');
+          log += 'b'
+          throw Error('omg')
         },
         c: function() {
-          log += 'c';
-          return Promise.resolve('!');
+          log += 'c'
+          return Promise.resolve('!')
         },
         d: function(arg) {
-          log += arg;
-          return Promise.resolve();
+          log += arg
+          return Promise.resolve()
         }
-      });
-    });
+      })
+    })
 
-    itBehavesLikePromiseBuilder();
+    itBehavesLikePromiseBuilder()
 
-  });
+  })
+
+  class Language {
+    x() {
+      log += 'x'
+      return Promise.resolve()
+    }
+    y() {
+      log += 'y'
+      return Promise.reject(Error('ouch'))
+    }
+    z() {
+      log += 'z'
+    }
+    a() {
+      log += 'a'
+      return this.x()
+    }
+    b() {
+      log += 'b'
+      throw Error('omg')
+    }
+    c() {
+      log += 'c'
+      return Promise.resolve('!')
+    }
+    d(arg) {
+      log += arg
+      return Promise.resolve()
+    }
+  }
+
+  class LanguageSubclass extends Language {}
 
   context('with an ES6 class instance as the language', function() {
 
     beforeEach(function() {
-      class Language {
-        x() {
-          log += 'x';
-          return Promise.resolve();
-        }
-        y() {
-          log += 'y';
-          return Promise.reject(Error('ouch'));
-        }
-        z() {
-          log += 'z';
-        }
-        a() {
-          log += 'a';
-          return this.x();
-        }
-        b() {
-          log += 'b';
-          throw Error('omg');
-        }
-        c() {
-          log += 'c';
-          return Promise.resolve('!');
-        }
-        d(arg) {
-          log += arg;
-          return Promise.resolve();
-        }
-      }
-      builder = promiseBuilder(new Language());
-    });
+      builder = promiseBuilder(new Language())
+    })
 
-    itBehavesLikePromiseBuilder();
+    itBehavesLikePromiseBuilder()
 
-  });
+  })
+
+  context('with an ES6 subclass instance as the language', function() {
+
+    beforeEach(function() {
+      builder = promiseBuilder(new LanguageSubclass())
+    })
+
+    itBehavesLikePromiseBuilder()
+
+  })
 
   function itBehavesLikePromiseBuilder() {
 
     it('is a promise', function() {
       return builder
         .then(function() {
-          expect(log).to.equal('');
-        });
-    });
+          expect(log).to.equal('')
+        })
+    })
 
     it('returns promises', function() {
       return builder.x()
         .then(function() {
-          expect(log).to.equal('x');
-        });
-    });
+          expect(log).to.equal('x')
+        })
+    })
 
     it('passes the result to the callback', function() {
       return builder.c()
         .then(function(result) {
-          expect(result).to.equal('!');
-        });
-    });
+          expect(result).to.equal('!')
+        })
+    })
 
     it('returns catchable promises', function() {
       return builder.y()
         .then(function() {
-          expect.fail('y is rejected!');
+          expect.fail('y is rejected!')
         })
         .catch(function() {
-          expect(log).to.equal('y');
-        });
-    });
+          expect(log).to.equal('y')
+        })
+    })
 
     it('calls an error callback passed to then', function() {
       return builder.y()
         .then(function() {
-          expect.fail('y is rejected!');
+          expect.fail('y is rejected!')
         }, function() {
-          expect(log).to.equal('y');
-        });
-    });
+          expect(log).to.equal('y')
+        })
+    })
 
     it('executes promises multiple times in the same chain', function() {
       return builder.x().x().x()
         .then(function() {
-          expect(log).to.equal('xxx');
-        });
-    });
+          expect(log).to.equal('xxx')
+        })
+    })
 
     it('executes promises when the language returns undefined', function() {
       return builder.z()
         .then(function() {
-          expect(log).to.equal('z');
-        });
-    });
+          expect(log).to.equal('z')
+        })
+    })
 
     it('sets "this" to the language definition object', function() {
       return builder.a()
         .then(function() {
-          expect(log).to.equal('ax');
-        });
-    });
+          expect(log).to.equal('ax')
+        })
+    })
 
     it('passes the result to the next step', function() {
       return builder.c().d()
         .then(function() {
-          expect(log).to.equal('c!');
-        });
-    });
+          expect(log).to.equal('c!')
+        })
+    })
 
     it('rejects promises that throw', function() {
       return builder.b()
         .then(function() {
-          expect.fail('b throws!');
+          expect.fail('b throws!')
         })
         .catch(function(e) {
-          expect(e.message).to.equal('omg');
-        });
-    });
+          expect(e.message).to.equal('omg')
+        })
+    })
 
     it('does not execute promises after a rejected promise', function() {
       return builder.x().y().z()
         .then(function() {
-          expect.fail('y is rejected!');
+          expect.fail('y is rejected!')
         })
         .catch(function(e) {
-          expect(e.message).to.equal('ouch');
-          expect(log).to.equal('xy');
-        });
-    });
+          expect(e.message).to.equal('ouch')
+          expect(log).to.equal('xy')
+        })
+    })
 
   }
 
   it('accepts a Promise implementation', function() {
-    var y = undefined;
-    var SpecialPromise = function() {};
-    SpecialPromise.resolve = function() { return Promise.resolve(321); }
+    var y = undefined
+    var SpecialPromise = function() {}
+    SpecialPromise.resolve = function() { return Promise.resolve(321) }
     var builder = promiseBuilder({
-      a: x => { y = x; return y; }
-    }, SpecialPromise);
+      a: function(x) {
+        y = x
+        return y
+      }
+    }, SpecialPromise)
     return builder.a()
       .then(function(result) {
-        expect(y).to.equal(321);
-      });
-  });
+        expect(y).to.equal(321)
+      })
+  })
 
-});
+})
